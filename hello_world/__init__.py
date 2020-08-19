84269e479a5adebf965ef5fa699f281986f055e8
Keenan_McKenzie <keenanmckenzie@lqdfinance.com>
2020-08-19 12:54:32 -0500

    Add models

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 from hello_world import blueprint_1
+from hello_world.models.price_model import run_price_model
 
 from hello_world.blueprint_1.routes import mod
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/price', methods=['POST'])
+def price_model():
+    json_body = request.get_json(force=True)
+    prediction = run_price_model(json_body)
+    return {'model': 'price_model', 'prediction': prediction}
---
+++ hello_world/models/price_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Import libraries
+import pandas as pd
+from sklearn import linear_model
+from joblib import dump, load
+
+
+#loading data - change to json later
+
+
+##TODO: Read json into df
+
+def run_price_model(json):
+    df = pd.json_normalize(json)
+    # input
+    x = df[['Units', 'SQ']]
+    # output
+    y = df['Price']
+
+    # load model
+    clf = load(r'decision_system\price_model\models\PriceModel.joblib') 
+
+    # Make predictions
+    y_pred = clf.predict(x)
+
+    return str(y_pred)
+
+
